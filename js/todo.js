35c08cf909202694d1691751f53a683d3d5b42da
ucom96 <57552154+ucom96@users.noreply.github.com>
2022-03-26 18:30:19 +0900

    todo-list

--- js/todo.js
+++ js/todo.js
@@ -8,7 +8,28 @@ const todoList = document.querySelector("#todo-list");
 function onTodoSubmit(event) {
   event.preventDefault();
   const todo = todoInput.value;
+  paintTodo(todo);
   todoInput.value = "";
+}
+
+function deleteTodo(event) {
+  //어떤 버튼을 누른건지 인식
+  const li = event.target.parentNode;
+  li.remove();
+}
+
+function paintTodo(todo) {
+  //html에 li 태그와 그 안에 span 태그를 만들어 todo value를 출력할것
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = todo;
+  const button = document.createElement("button");
+  button.innerText = `❌`;
+  //button 클릭시 li가 사라지도록
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
+  todoList.appendChild(li);
 }
 
 todoForm.addEventListener("submit", onTodoSubmit);
