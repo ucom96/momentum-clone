4f679ff63b3c620ea49a81cd77f118be2a616e1e
ucom96 <57552154+ucom96@users.noreply.github.com>
2022-04-02 19:35:44 +0900

    new

--- js/background.js
+++ js/background.js
@@ -1,9 +1,8 @@
 const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"];
-
 //html에 image 태그 추가하기
 const imgEl = document.createElement("img");
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 imgEl.src = `img/${chosenImage}`;
-
+imgEl.classList.add("bgImg");
 //body에 넣기
 document.body.appendChild(imgEl);
